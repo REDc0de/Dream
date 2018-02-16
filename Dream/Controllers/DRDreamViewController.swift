257f94aad87a52fe9d07f7fb36dd7e73943eec20
Bogdan Chaikovsky <bogdan.chaikovsky@gmail.com>
2018-02-17 02:36:26 +0300

    Added controllers to screens. Added navigation to the application.

---
+++ Dream/Controllers/DRDreamViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DRDreamViewController.swift
+//  Dream
+//
+//  Created by Bogdan Chaikovsky on 17.02.18.
+//  Copyright © 2018 Bogdan Chaikovsky. All rights reserved.
+//
+
+import UIKit
+
+class DRDreamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
