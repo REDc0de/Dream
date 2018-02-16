bea24c8d14011895134035d3728f7a491a6e18b2
Bogdan Chaikovsky <bogdan.chaikovsky@gmail.com>
2018-02-17 02:11:03 +0300

    Created simple CoreData manager.

---
+++ Dream/CoreDataManager.swift
@@ -0,0 +1,72 @@
+//
+//  CoreDataManager.swift
+//  Dream
+//
+//  Created by Bogdan Chaikovsky on 17.02.18.
+//  Copyright © 2018 Bogdan Chaikovsky. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataManager {
+    
+    // MARK: - Constants
+    
+    static let sharedInstance = CoreDataManager()
+    
+    fileprivate let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    // MARK: - Methods
+    
+    public func addDream(uuid: String, name: String, startDate: Date, targetDate: Date, currentCredits: Double, targetCredits: Double, image: Data, info: String) {
+        guard let entity = NSEntityDescription.entity(forEntityName: "Dream", in: managedObjectContext) else {
+            print("CoreDataManager: add entity error")
+            
+            return
+        }
+
+        let dream = NSManagedObject(entity: entity, insertInto: managedObjectContext)
+        
+        dream.setValue(uuid,           forKeyPath: "uuid"          )
+        dream.setValue(name,           forKeyPath: "name"          )
+        dream.setValue(startDate,      forKeyPath: "startDate"     )
+        dream.setValue(targetDate,     forKeyPath: "targetDate"    )
+        dream.setValue(currentCredits, forKeyPath: "currentCredits")
+        dream.setValue(targetCredits,  forKeyPath: "targetCredits" )
+        dream.setValue(image,          forKeyPath: "image"         )
+        dream.setValue(info,           forKeyPath: "info"          )
+    }
+    
+    public func addTransaction(uuid: String, dream: Dream, date: Date, credits: Double) {
+        guard let entity = NSEntityDescription.entity(forEntityName: "Transaction", in: managedObjectContext) else {
+            print("CoreDataManager: add entity error")
+            
+            return
+        }
+        
+        let transaction  = NSManagedObject(entity: entity, insertInto: managedObjectContext)
+        
+        transaction.setValue(uuid,    forKeyPath: "uuid"    )
+        transaction.setValue(date,    forKeyPath: "date"    )
+        transaction.setValue(dream,   forKeyPath: "dream"   )
+        transaction.setValue(credits, forKeyPath: "credits" )
+    }
+    
+    public func fetchDreams() -> [Dream] {
+        
+        let fetchRequest = NSFetchRequest<Dream>(entityName: "Dream")
+        
+        do {
+            let dreams = try managedObjectContext.fetch(fetchRequest)
+            return dreams
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        return [Dream]()
+    }
+    
+    public func deleteDream(_ object: Dream) {
+        managedObjectContext.delete(object)
+    }
+}
