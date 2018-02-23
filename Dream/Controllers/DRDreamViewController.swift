8c93563f45b33a6360ab8c0df8c1f76dc1671213
Bogdan Chaikovsky <bogdan.chaikovsky@gmail.com>
2018-02-23 23:38:07 +0300

    Added money controller for DRAddController class.

--- Dream/Controllers/DRDreamViewController.swift
+++ Dream/Controllers/DRDreamViewController.swift
@@ -51,50 +51,23 @@ class DRDreamViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Actions
     
-    @IBAction func add(_ sender: UITapGestureRecognizer) {
+    @IBAction func minus(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func plus(_ sender: UIButton) {
         
-        var amountTextField: UITextField?
-        
-        // Declare Alert message
-        let dialogMessage = UIAlertController(title: "Add money", message: "Please enter amount of money you want to add.", preferredStyle: .alert)
-        
-        // Create OK button with action handler
-        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
-            print("Ok button tapped")
-            
-            if let userInput = amountTextField!.text {
-                
-                self.dream?.currentCredits = (self.dream?.currentCredits ?? 0.0) + (Double(userInput) ?? 0.0)
-                CoreDataManager.sharedInstance.addTransaction(uuid: UUID().uuidString, dream: self.dream!, date: Date(), credits: Double(userInput)!)
-                
-                CoreDataManager.sharedInstance.saveContext()
-                
-                self.progressView.dream = self.dream
-                
-                print("User entered \(userInput)")
-            }
-        })
-        
-        // Create Cancel button with action handlder
-        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) -> Void in
-            print("Cancel button tapped")
-        }
-        
-        //Add OK and Cancel button to dialog message
-        dialogMessage.addAction(ok)
-        dialogMessage.addAction(cancel)
-        
-        // Add Input TextField to dialog message
-        dialogMessage.addTextField { (textField) -> Void in
-            
-            amountTextField = textField
-            amountTextField?.keyboardType = .numberPad
-            amountTextField?.placeholder  = "Money amount"
-        }
-        
-        // Present dialog message to user
-        self.present(dialogMessage, animated: true, completion: nil)
-        
+    }
+    
+    @IBAction func add(_ sender: UIButton) {
+
+//                self.dream?.currentCredits = (self.dream?.currentCredits ?? 0.0) + (Double(userInput) ?? 0.0)
+//                CoreDataManager.sharedInstance.addTransaction(uuid: UUID().uuidString, dream: self.dream!, date: Date(), credits: Double(userInput)!)
+//                
+//                CoreDataManager.sharedInstance.saveContext()
+//                
+//                self.progressView.dream = self.dream
+     
     }
     
     // MARK: - Navigation
