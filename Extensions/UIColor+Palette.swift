77be191591584166036b261cbdbca4a8170b6872
Bogdan Chaikovsky <bogdan.chaikovsky@gmail.com>
2018-02-27 23:41:28 +0300

    Added colors to UIColor+Palette.

--- Extensions/UIColor+Palette.swift
+++ Extensions/UIColor+Palette.swift
@@ -13,4 +13,12 @@ extension UIColor {
     public static let intensiveRed = UIColor(red: 255/255, green: 0/255,   blue: 48/255,  alpha: 1.0)
     public static let purpleRed    = UIColor(red: 231/255, green: 212/255, blue: 220/255, alpha: 1.0)
     
+    // RED
+    public static let monza       = UIColor(red: 207/255, green: 0/255,   blue: 15/255,  alpha: 1.0)
+    public static let pomegranate = UIColor(red: 242/255, green: 38/255,  blue: 19/255,  alpha: 1.0)
+    public static let thunderbird = UIColor(red: 217/255, green: 30/255,  blue: 24/255,  alpha: 1.0)
+    // Green
+    public static let malachite   = UIColor(red: 0/255,   green: 230/255, blue: 64/255,  alpha: 1.0)
+    public static let shamrock    = UIColor(red: 46/255,  green: 204/255, blue: 113/255, alpha: 1.0)
+    public static let eucalyptus  = UIColor(red: 36/255,  green: 166/255, blue: 91/255,  alpha: 1.0)
 }
