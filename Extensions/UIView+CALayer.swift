5e6bb0b411e9abf3bcb9aaaa1cc03008ffbe1091
Bogdan Chaikovsky <bogdan.chaikovsky@gmail.com>
2018-03-10 02:33:44 +0300

    Implemented UIView+CALayer extension.

--- Extensions/UIView+CALayer.swift
+++ Extensions/UIView+CALayer.swift
@@ -8,28 +8,43 @@
 
 import UIKit
 
-class UIView_CALayer: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+@IBDesignable
+extension UIView {
+    
+    @IBInspectable
+    var borderWidth: Float {
+        get {
+            return Float(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
+            self.setNeedsDisplay()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBInspectable
+    var cornerRadius: Float {
+        get {
+            return Float(self.layer.cornerRadius)
+        }
+        set {
+            self.layer.cornerRadius = CGFloat(newValue)
+            self.setNeedsDisplay()
+        }
     }
-    */
-
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            guard let color = self.layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+            self.setNeedsDisplay()
+        }
+    }
+    
 }
