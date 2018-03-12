1af21b570927f6566a38cddeb2b48b8b2d382ca4
Bogdan Chaikovsky <bogdan.chaikovsky@gmail.com>
2018-03-12 23:20:14 +0300

    Refactored Double+Progress extension.

--- Extensions/Double+Progress.swift
+++ Extensions/Double+Progress.swift
@@ -11,27 +11,21 @@ import Foundation
 extension Double {
     
     public func progress(between startDate: Date?, and targetDate: Date?) -> Double {
-        guard let startDate = startDate, let targetDate = targetDate else { return 0 }
+        guard let startDate = startDate, let targetDate = targetDate else {
+            return 0
+        }
         
-        var progress = Double((Date().timeIntervalSinceNow - startDate.timeIntervalSinceNow) / (targetDate.timeIntervalSinceNow - startDate.timeIntervalSinceNow))
-        
-        if progress.isNaN { progress = 0 }
-        if progress < 0   { progress = 0 }
-        if progress > 1   { progress = 1 }
-        
-        return progress
+        let progress = Double((Date().timeIntervalSinceNow - startDate.timeIntervalSinceNow) / (targetDate.timeIntervalSinceNow - startDate.timeIntervalSinceNow))
+
+        return progress.isNaN || progress < 0 ? 0 : progress > 1 ? 1 : progress
     }
     
     public func progress(between number: Double?, and targetNumber: Double?) -> Double {
         guard let number = number, let targetNumber = targetNumber else { return 0 }
         
-        var progress = number/targetNumber
-        
-        if progress.isNaN { progress = 0 }
-        if progress < 0   { progress = 0 }
-        if progress > 1   { progress = 1 }
-        
-        return progress
+        let progress = number/targetNumber
+
+        return progress.isNaN || progress < 0 ? 0 : (progress > 1 ? 1 : progress)
     }
     
 }
